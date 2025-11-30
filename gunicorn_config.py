1e0d20b2066a366d1eb8174cb68236d32ecd4dfb
rupper <mehrzad.mobin@gmail.com>
2025-11-30 12:25:44 +0100

    Add server configuration (Gunicorn, Procfile)

---
+++ gunicorn_config.py
@@ -0,0 +1,13 @@
+import os
+
+bind = "0.0.0.0:8000"
+workers = 1
+worker_class = "eventlet"
+threads = 1
+timeout = 120
+keepalive = 5
+
+# Logging
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
